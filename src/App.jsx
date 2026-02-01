88ebcd99c1fc4fdbe39f6eb6cc40304ad9679abe
Siddharaj Ghorpade <siddharaj@gmail.com>
2026-02-01 20:30:11 +0530

    updated project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import LoginSignup from './Components/LoginSignup/LoginSignup'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+          <div>
+      <LoginSignup />
+    </div>
+    </>
+  )
+}
+
+export default App
